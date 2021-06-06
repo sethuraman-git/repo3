61deb3b074dbecdb24d826ae9475bd7f9b3e0347
sethuraman.git <seduramanvictory@gmail.com>
2021-06-06 16:48:27 +0530

    add

--- src/org/gitpac/GitClass.java
+++ src/org/gitpac/GitClass.java
@@ -1,9 +1,13 @@
 package org.gitpac;
 
 public class GitClass {
+	
 	public static void main(String[] args) {
+		
 		System.out.println("master");
 		System.out.println("branch1");
+		System.out.println("new");
+		System.out.println("sdjabn");
 	}
 
 }
