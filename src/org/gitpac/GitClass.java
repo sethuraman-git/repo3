450cd1a265359667a136da52420f25ac1dfcbffb
sethuraman.git <seduramanvictory@gmail.com>
2021-06-06 09:53:27 +0530

    br2 added

--- src/org/gitpac/GitClass.java
+++ src/org/gitpac/GitClass.java
@@ -1,8 +1,13 @@
 package org.gitpac;
 
 public class GitClass {
+	private void br2() {
+		System.out.println("br2");
+	}
 	public static void main(String[] args) {
 		System.out.println("master");
+		GitClass g = new GitClass();
+		g.br2();
 	}
 
 }
